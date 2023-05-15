24b7cfc1fde7058488f98c61a9366bcf92d5d84c
Rodrigo <rorro.poblete@gmail.com>
2023-05-15 18:12:16 -0400

    add PHP section

---
+++ 05-PHP/introduccionPHP/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introducción a PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+    <div class="contenido">
+        <h1>Lenguaje de programación PHP</h1>
+        <p>Introducción a PHP</p>
+
+        <div class="resultado">
